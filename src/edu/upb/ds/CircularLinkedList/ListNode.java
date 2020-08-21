602ff109572cd6f2fd6dd6acfbcfc7a7f01a71f1
Pedro Felipe Gomez <pedro.gomez.2019@upb.edu.co>
2020-08-21 16:32:34 -0500

    Initial commit

---
+++ src/edu/upb/ds/CircularLinkedList/ListNode.java
@@ -0,0 +1,11 @@
+package edu.upb.ds.CircularLinkedList;
+
+public class ListNode {
+    private Object object;
+    private ListNode next;
+
+    public void ListNode(){
+        this.object=object;
+        this.next=null;
+    }
+}
